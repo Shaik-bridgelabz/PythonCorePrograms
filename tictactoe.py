87c63fc9c74439f45ecaacdd3e9f999d58553fc7
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-18 00:11:44 +0530

    [Shaik] Added. Function to Display the board to player.

--- tictactoe.py
+++ tictactoe.py
@@ -6,6 +6,7 @@ def reset_board():
     print('Welcome to Tic Tac Toe!')
     # "board" is a list of 10 strings representing the board (ignore index 0)
     board = [' '] * 10
+    return board
 
 
 def input_player_letter():
@@ -32,7 +33,20 @@ def toss():
         return 'player'
 
 
+def draw_board():
+
+    # This function prints out the board that it was passed.
+    # "board" is a list of 10 strings representing the board (ignore index 0)
+    board = [' ']*10
+    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print('-----------')
+    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print('-----------')
+    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+
+
 reset_board()
 input_player_letter()
 toss()
+draw_board()
 
