c689470dca681d78f6c33397cff15bd75dce9654
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-18 15:43:07 +0530

    [Shaik] Refactored. Code and Test to take message in Constructor.

--- mood_analyser.py
+++ mood_analyser.py
@@ -1,7 +1,10 @@
 class MoodAnalyser:
 
-    def analysemood(self, message):
-        if "Sad" in message:
+    def __init__(self, message):
+        self.message = message
+
+    def analysemood(self):
+        if "Sad" in self.message:
             return "SAD"
         else:
             return "HAPPY"
