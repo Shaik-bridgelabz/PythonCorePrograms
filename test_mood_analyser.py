6ac2a0cc8befe9c6843458755c1f83fff1cd824f
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-19 00:58:42 +0530

    [Shaik] Added. to Handle Null scenario using Custom exception.

--- test_mood_analyser.py
+++ test_mood_analyser.py
@@ -1,3 +1,5 @@
+import null as null
+
 from mood_analyser import MoodAnalyser
 
 
@@ -6,12 +8,17 @@ def test_analysemood_whengivenmessage_sad_shouldreturn_sad():
     mood = moodAnalyser.analysemood()
     assert mood == "SAD"
 
+
 def test_analysemood_whengivenmessage_happy_shouldreturn_happy():
     moodAnalyser = MoodAnalyser("Iam in Happy Mood")
     mood = moodAnalyser.analysemood()
     assert mood == "HAPPY"
 
-def test_analysemood_whengivennullmessage_shouldreturn_happy():
-    moodAnalyser = MoodAnalyser("null")
-    mood = moodAnalyser.analysemood()
-    assert mood == "HAPPY"
+
+def test_analysemood_whengivennullmessage_shouldreturn_exception():
+    from mood_analyser_exception import MoodAnalyserException
+    try:
+        moodAnalyser = MoodAnalyser("null")
+        moodAnalyser.analysemood()
+    except MoodAnalyserException as e:
+        assert e.type == TypeError
