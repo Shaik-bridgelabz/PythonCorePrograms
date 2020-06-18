12494439de8b347708d82cc9ad2aa7e740f197f3
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-18 15:35:21 +0530

    [Shaik] Added. Function to analyse mood and return HAPPY.

--- test_mood_analyser.py
+++ test_mood_analyser.py
@@ -1,7 +1,12 @@
 from mood_analyser import MoodAnalyser
 
 
-def test_analysemood():
+def test_analysemood_whengivenmessage_sad_shouldreturn_sad():
     moodAnalyser = MoodAnalyser()
     mood = moodAnalyser.analysemood("Iam in Sad Mood")
     assert mood == "SAD"
+
+def test_analysemood_whengivenmessage_happy_shouldreturn_happy():
+    moodAnalyser = MoodAnalyser()
+    mood = moodAnalyser.analysemood("Iam in Happy Mood")
+    assert mood == "HAPPY"
