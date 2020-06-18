779acf3f2e65ad40dffcdb7cb169f58abe1361e7
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-18 15:43:07 +0530

    [Shaik] Refactored. Code and Test to take message in Constructor.

--- test_mood_analyser.py
+++ test_mood_analyser.py
@@ -2,11 +2,11 @@ from mood_analyser import MoodAnalyser
 
 
 def test_analysemood_whengivenmessage_sad_shouldreturn_sad():
-    moodAnalyser = MoodAnalyser()
-    mood = moodAnalyser.analysemood("Iam in Sad Mood")
+    moodAnalyser = MoodAnalyser("Iam in Sad Mood")
+    mood = moodAnalyser.analysemood()
     assert mood == "SAD"
 
 def test_analysemood_whengivenmessage_happy_shouldreturn_happy():
-    moodAnalyser = MoodAnalyser()
-    mood = moodAnalyser.analysemood("Iam in Happy Mood")
+    moodAnalyser = MoodAnalyser("Iam in Happy Mood")
+    mood = moodAnalyser.analysemood()
     assert mood == "HAPPY"
