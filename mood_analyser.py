6218086dbf0f066e19a652055fac13996a8fbcab
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-19 20:23:11 +0530

    [Shaik] Refactored. to handle custom exception .

--- mood_analyser.py
+++ mood_analyser.py
@@ -6,14 +6,13 @@ class MoodAnalyser:
     def analysemood(self):
         from mood_analyser_exception import MoodAnalyserException
         try:
+            if self.message is None:
+                raise MoodAnalyserException(error=TypeError, message="Entered Null Value")
             if len(self.message) == 0:
                 raise MoodAnalyserException(error=NameError, message="Entered empty Value")
             if "Sad" in self.message:
                 return "SAD"
             else:
                 return "HAPPY"
-        except MoodAnalyserException as e:
-            return e.error
-
-
-
+        finally:
+            return "No error"
