6783be38bd2a1bfd36bf03283a3cd0d3d2f923d7
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-20 22:25:24 +0530

    [Shaik] Added. Code for operations in a Set.

--- sets.py
+++ sets.py
@@ -7,3 +7,13 @@ my_set = {1, 2, 3}
 my_set.add(4)  # add element to set
 my_set.add(8)
 print(my_set)
+
+# Operation in Sets
+my_set = {1, 2, 3, 4}
+my_set_2 = {3, 4, 5, 6}
+print(my_set.union(my_set_2), '----------', my_set | my_set_2)
+print(my_set.intersection(my_set_2), '----------', my_set & my_set_2)
+print(my_set.difference(my_set_2), '----------', my_set - my_set_2)
+print(my_set.symmetric_difference(my_set_2), '----------', my_set ^ my_set_2)
+my_set.clear()
+print(my_set)
