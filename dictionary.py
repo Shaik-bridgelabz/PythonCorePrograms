546e9679bd15c77569335fa1874e98b57721208b
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-20 21:41:54 +0530

    [Shaik]Added.Code to change and add elements in a Dictionary.

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,12 @@ my_dict = {}  # empty dictionary
 print(my_dict)
 my_dict = {1: 'Python', 2: 'Java', 3: 'SQL', 4: 'HTML'}  # dictionary with elements
 print(my_dict)
+
+# Changing and Adding Elements into Dictionary
+my_dict = {'First': 'Python', 'Second': 'Java'}
+print(my_dict)
+my_dict['Second'] = 'C++'  # changing element
+print(my_dict)
+my_dict['Third'] = 'Ruby'  # adding key-value pair
+my_dict['Four'] = '.Net'  # adding key-value pair
+print(my_dict)
