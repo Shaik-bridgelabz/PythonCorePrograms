dddbb0a6a5d028abc33a1576396cd7a6136e57a9
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-20 19:18:42 +0530

    [Shaik] Added. Code to delete elements from the list.

--- list.py
+++ list.py
@@ -12,3 +12,14 @@ my_list.extend([234, 'more_example'])  # add as different elements
 print(my_list)
 my_list.insert(1, 'insert_example')  # add element at index position
 print(my_list)
+
+# Deleting Elements
+my_list = [1, 2, 3, 'example', 3.132, 10, 30]
+del my_list[5]  # delete element at index 5
+print(my_list)
+my_list.remove('example')  # remove element with value
+print(my_list)
+a = my_list.pop(1)  # pop element from list
+print('Popped Element: ', a, ' List remaining: ', my_list)
+my_list.clear()  # empty the list
+print(my_list)
