93bf75b216e1742c3ace93faf22ee7c7c137c23b
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-20 19:24:11 +0530

    [Shaik] Added. Code for different list inbuilt Functions.

--- list.py
+++ list.py
@@ -32,3 +32,12 @@ print(my_list)  # access all elements
 print(my_list[3])  # access index 3 element
 print(my_list[0:2])  # access elements from 0 to 1 and exclude 2
 print(my_list[::-1])  # access elements in reverse
+
+# Other List Inbuilt Functions
+my_list = [1, 2, 3, 10, 30, 10]
+print(len(my_list))  # find length of list
+print(my_list.index(10))  # find index of element that occurs first
+print(my_list.count(10))  # find count of the element
+print(sorted(my_list))  # print sorted list but not change original
+my_list.sort(reverse=True)  # sort original list
+print(my_list)
