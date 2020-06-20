bcd312d23e5915e8f321d3a326473cb54540cfc2
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-20 22:23:19 +0530

    [Shaik] Added. Code to add elements to a Set.

--- sets.py
+++ sets.py
@@ -1,3 +1,9 @@
 # Creating a set
 my_set = {1, 2, 3, 4, 5, 5, 5}  # create set
 print(my_set)
+
+# Adding Elements
+my_set = {1, 2, 3}
+my_set.add(4)  # add element to set
+my_set.add(8)
+print(my_set)
