6ff6629200e4723c2b386f5cad61b83bef3e49a5
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-06-20 19:12:23 +0530

    [Shaik] Added. Code to add elements in the list.

--- list.py
+++ list.py
@@ -2,3 +2,13 @@ my_list = []  # create empty list
 print(my_list)
 my_list = [1, 2, 3, 'example', 3.132]  # creating list with data
 print(my_list)
+
+# Adding Elements
+my_list = [1, 2, 3]
+print(my_list)
+my_list.append([555, 12])  # add as a single element
+print(my_list)
+my_list.extend([234, 'more_example'])  # add as different elements
+print(my_list)
+my_list.insert(1, 'insert_example')  # add element at index position
+print(my_list)
